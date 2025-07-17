641bffcafe61cb77353e7b9b6b2a5defdcf02263
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-07-17 11:33:42 +0400

    Add Next.js Template (DevExtreme CLI v1.11.0)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# devextreme-nextjs-template
-Responsive Application Layout Templates​ based on DevExtreme React Components
+# DevExtreme Next.js Template
+
+The DevExtreme Next.js Template is a Next.js application with a navigation menu and sample views in a responsive layout. This application is created with [create-next-app](https://nextjs.org/docs/app/api-reference/cli/create-next-app) and uses [DevExtreme React components](https://js.devexpress.com/Documentation/Guide/React_Components/DevExtreme_React_Components/).
+
+![DevExtreme-Nextjs-Template](nextjs-template.png)
+
+## Getting Started
+
+For more information about the DevExtreme React Template and how to customize it, refer to the following help topic: [Application Template](https://js.devexpress.com/Documentation/Guide/React_Components/Application_Template/).
+
+## License
+
+**DevExtreme React Template is released as a MIT-licensed (free and open-source) add-on to DevExtreme.**
+
+- [DevExtreme License](https://js.devexpress.com/Licensing/)
+- [Free trial](http://js.devexpress.com/Buy/)
---
+++ src/middleware.jsx
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { decrypt, createSession } from '@/app/lib/session';
+import defaultUser from '@/utils/default-user';
+
+const isProtectedRoute = (path) => path.startsWith('/pages');
+
+async function _DEMO_logIn() {
+  await createSession(defaultUser.id);
+
+  return NextResponse.next();
+}
+
+async function redirectUnauthorized(req) {
+  return await _DEMO_logIn();
+
+  // In production, you will need to redirect unauthorized users
+  // return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
+}
+
+export default async function middleware(req) {
+  const path = req.nextUrl.pathname;
+
+  if (!isProtectedRoute(path)) {
+    return NextResponse.next();
+  }
+
+  const cookie = (await cookies()).get('session')?.value;
+
+  if (!cookie) {
+    return await redirectUnauthorized(req);
+  }
+
+  const session = await decrypt(cookie);
+
+  if (!session?.userId) {
+    return await redirectUnauthorized(req);
+  }
+ 
+  return NextResponse.next();
+}
+ 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
